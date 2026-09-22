9c0b3a914ffa7300927c9da7c57c92fb90aef510
pakerjaehyeong <paker1215@gmail.com>
2026-09-22 18:53:13 +0900

    2026.9.22(Class)

---
+++ Class2th (Class)/source.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+namespace Brazil
+{
+	int population = 21340000;
+	float area = 8515767;
+	void informatoin() {
+		cout << "area:" << area << "km" << endl;
+		cout << "population:" << population << endl;
+	}
+
+}
+
+namespace Colombia
+{
+	int population = 32130000;
+	float area = 1141748;
+	void informatoin() {
+		cout << "area:" << area << "km" << endl;
+		cout << "population:" << population << endl;
+	}
+
+
+}
+
+using namespace Brazil;
+
+class Charator
+{
+#pragma region 접근 지정자
+	// 클래스 내부에 포함되어 있는 속성에 접근 범위를 
+	// 제한하는 제한자 입니다
+
+	// public : 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스
+	//			외부에서도 접근을 허용하는 지정자입니다
+
+	// protected: 클래스 내부와 자기가 상속하고 있는 클래스까지만
+	//			  접근을 허용하는 지정자 입니다
+
+	// private : 클래스 내부까지만 접근을 허용하는 지정자입니다
+
+	// default 접근 지정자는 private로 잡힌다
+#pragma endregion
+
+private:
+	int x = 1;
+	int y = 2;
+	int z = 3;
+
+protected:
+	const char* name;
+public:
+
+	void Position()
+	{
+		cout << "x:" << x << endl;
+		cout << "y:" << y << endl;
+		cout << "z:" << z << endl;
+	};
+};
+
+void Profile(const char* name = "James", int age = 20)
+{
+	cout << "name:" << name << endl;
+	cout << " age:" << age << endl;
+}
+
+
+int main()
+{
+#pragma region 이름 공간
+	//속성을 구분할 수 있도록 유효 범위를 
+	// 설정하는 영역입니다
+	// 같은 변수나 함수명을 쓰더라도 다른 범위를 지정해 구분시킬수 있다
+	// std의 경우에도 기본적으로 선언된 namespace이다 
+
+	// informatoin();
+	// Colombia::informatoin();
+
+
+#pragma endregion
+
+#pragma region 클래스
+	// 사용자 정의 데이터 유형으로 속성과 함수가 포함되어
+	// 있으며, 클래스를 통해 객체를 생성하여 접근하고 사용 
+	// 하는 집합체입니다
+
+	// Charator charator;
+	// 
+	// charator.Position();
+	// 
+	// cout << "charator of Size:" << sizeof(Charator) << endl;
+
+
+	// 클래스의 경우 클래스 내부에 있는 변수는 클래스의 
+	// 메모리 영역에 포함되지만, 정적 변수와 함수의 메모리는
+	// 클래스 영역에 포함되지 않습니다
+
+
+
+
+
+#pragma endregion
+
+#pragma region 기본 매개 변수
+	//함수의 매개 변수에 값이 전달되지 않을 때
+	// 미리 값을 설정해놓은 매개 변수입니다
+
+	// Profile();
+	// Profile("John",24);
+
+	// 기본 매개 변수를 지정하려면 오른쪽에서 부터 
+	// 값을 지정해주어야 합니다
+#pragma endregion
+
+
+
+
+}
